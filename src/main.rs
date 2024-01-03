bdaeb96244f7752f661f8b3a0df079a7bec5c0ea
Rn86222 <rn86222.light0213@gmail.com>
2024-01-03 22:40:45 +0900

    example of morphological analysis

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,142 @@ mod bayesian_network;
 use bayesian_network::*;
 use std::collections::HashMap;
 
+fn example_of_morphological_analysis() {
+    println!("Time flies like an arrow の品詞解析");
+    let noun = "名詞";
+    let verb = "動詞";
+    let adjective = "形容詞";
+    let article = "冠詞";
+    let preposition = "前置詞";
+    let mut network = BayesianNetwork::new(vec![
+        noun,
+        verb,
+        adjective,
+        article,
+        preposition,
+        "time",
+        "flies",
+        "like",
+        "an",
+        "arrow",
+    ]);
+
+    let mut bos_prob_map = HashMap::new();
+    bos_prob_map.insert(noun, 0.6);
+    bos_prob_map.insert(article, 0.4);
+    network.add_node("TimePart", NodeType::Root(bos_prob_map));
+    network.add_node("TimeWord", NodeType::Leaf);
+    network.add_node("FliesPart", NodeType::Inner);
+    network.add_node("FliesWord", NodeType::Leaf);
+    network.add_node("LikePart", NodeType::Inner);
+    network.add_node("LikeWord", NodeType::Leaf);
+    network.add_node("AnPart", NodeType::Inner);
+    network.add_node("AnWord", NodeType::Leaf);
+    network.add_node("ArrowPart", NodeType::Inner);
+    network.add_node("ArrowWord", NodeType::Leaf);
+
+    let mut noun_prob_map = HashMap::new();
+    noun_prob_map.insert(noun, 0.3);
+    noun_prob_map.insert(verb, 0.4);
+    noun_prob_map.insert(adjective, 0.1);
+    noun_prob_map.insert(preposition, 0.2);
+
+    let mut verb_prob_map = HashMap::new();
+    verb_prob_map.insert(noun, 0.1);
+    verb_prob_map.insert(adjective, 0.5);
+    verb_prob_map.insert(article, 0.2);
+    verb_prob_map.insert(preposition, 0.2);
+
+    let mut adjective_prob_map = HashMap::new();
+    adjective_prob_map.insert(noun, 0.5);
+    adjective_prob_map.insert(adjective, 0.4);
+    adjective_prob_map.insert(article, 0.1);
+
+    let mut article_prob_map = HashMap::new();
+    article_prob_map.insert(noun, 0.7);
+    article_prob_map.insert(preposition, 0.3);
+
+    let mut preposition_prob_map = HashMap::new();
+    preposition_prob_map.insert(noun, 0.6);
+    preposition_prob_map.insert(adjective, 0.1);
+    preposition_prob_map.insert(article, 0.3);
+
+    let mut a_prob_map = HashMap::new();
+    a_prob_map.insert(vec![noun], noun_prob_map);
+    a_prob_map.insert(vec![verb], verb_prob_map);
+    a_prob_map.insert(vec![adjective], adjective_prob_map);
+    a_prob_map.insert(vec![article], article_prob_map);
+    a_prob_map.insert(vec![preposition], preposition_prob_map);
+
+    network.add_dependency(vec!["TimePart"], "FliesPart", a_prob_map.clone());
+    network.add_dependency(vec!["FliesPart"], "LikePart", a_prob_map.clone());
+    network.add_dependency(vec!["LikePart"], "AnPart", a_prob_map.clone());
+    network.add_dependency(vec!["AnPart"], "ArrowPart", a_prob_map);
+
+    let mut noun_b_prob_map = HashMap::new();
+    noun_b_prob_map.insert("time", 0.6);
+    noun_b_prob_map.insert("arrow", 0.3);
+    noun_b_prob_map.insert("flies", 0.1);
+
+    let mut verb_b_prob_map = HashMap::new();
+    verb_b_prob_map.insert("like", 0.7);
+    verb_b_prob_map.insert("arrow", 0.1);
+    verb_b_prob_map.insert("flies", 0.2);
+
+    let mut adjective_b_prob_map = HashMap::new();
+    adjective_b_prob_map.insert("like", 1.0);
+
+    let mut article_b_prob_map = HashMap::new();
+    article_b_prob_map.insert("an", 1.0);
+
+    let mut preposition_b_prob_map = HashMap::new();
+    preposition_b_prob_map.insert("like", 1.0);
+
+    let mut b_prob_map = HashMap::new();
+    b_prob_map.insert(vec![noun], noun_b_prob_map);
+    b_prob_map.insert(vec![verb], verb_b_prob_map);
+    b_prob_map.insert(vec![adjective], adjective_b_prob_map);
+    b_prob_map.insert(vec![article], article_b_prob_map);
+    b_prob_map.insert(vec![preposition], preposition_b_prob_map);
+
+    network.add_dependency(vec!["TimePart"], "TimeWord", b_prob_map.clone());
+    network.add_dependency(vec!["FliesPart"], "FliesWord", b_prob_map.clone());
+    network.add_dependency(vec!["LikePart"], "LikeWord", b_prob_map.clone());
+    network.add_dependency(vec!["AnPart"], "AnWord", b_prob_map.clone());
+    network.add_dependency(vec!["ArrowPart"], "ArrowWord", b_prob_map);
+
+    let mut evidence = HashMap::new();
+    evidence.insert("TimeWord", "time");
+    evidence.insert("FliesWord", "flies");
+    evidence.insert("LikeWord", "like");
+    evidence.insert("AnWord", "an");
+    evidence.insert("ArrowWord", "arrow");
+
+    let inferred_probabilities = network.infer(&evidence);
+    println!(
+        "Time  名詞の確率: {}",
+        network.get_inferred_probability(&inferred_probabilities, "TimePart", noun)
+    );
+    println!(
+        "flies 動詞の確率: {}  名詞の確率: {}",
+        network.get_inferred_probability(&inferred_probabilities, "FliesPart", verb),
+        network.get_inferred_probability(&inferred_probabilities, "FliesPart", noun)
+    );
+    println!(
+        "like  前置詞の確率: {} 動詞の確率: {}",
+        network.get_inferred_probability(&inferred_probabilities, "LikePart", preposition),
+        network.get_inferred_probability(&inferred_probabilities, "LikePart", verb)
+    );
+    println!(
+        "an    冠詞の確率: {}",
+        network.get_inferred_probability(&inferred_probabilities, "AnPart", article)
+    );
+    println!(
+        "arrow 名詞の確率: {}",
+        network.get_inferred_probability(&inferred_probabilities, "ArrowPart", noun)
+    );
+}
+
 fn main() {
     let mut network = BayesianNetwork::new(vec![true, false]);
 
@@ -88,4 +224,6 @@ fn main() {
     );
 
     println!("{:?}", network);
+
+    example_of_morphological_analysis();
 }
